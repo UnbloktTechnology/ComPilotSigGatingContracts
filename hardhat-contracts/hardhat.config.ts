ef51f32537cba34a12efbe57ce73d8220265c608
Antoine Estienne <estienne.antoine@gmail.com>
2023-12-13 19:15:21 +0100

    install packages and build

--- hardhat-contracts/hardhat.config.ts
+++ hardhat-contracts/hardhat.config.ts
@@ -6,7 +6,7 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "@openzeppelin/hardhat-upgrades";
-import "@nomicfoundation/hardhat-viem";
+//import "@nomicfoundation/hardhat-viem";
 
 //import "@nomicfoundation/hardhat-toolbox-viem";
 
