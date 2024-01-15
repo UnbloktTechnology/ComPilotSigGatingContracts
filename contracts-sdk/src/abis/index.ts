755f653fd1e9dd473823f15fd21f538fbd62102b
Antoine Estienne <estienne.antoine@gmail.com>
2024-01-15 14:48:01 +0100

    reoganize contract sdk into mutiple entrypoints (#8)

---
+++ contracts-sdk/src/abis/index.ts
@@ -0,0 +1,7 @@
+// ABIs
+export * from "./CredentialAtomicQuerySigValidatorABI";
+export * from "./NexeraVerifierEntrypointABI";
+export * from "./ScenarioVerifierABI";
+export * from "./ScenarioVerifierFactoryABI";
+export * from "./VerifierEntrypointFactoryABI";
+export * from "./VerifierSigWrapperABI";
---
+++ contracts-sdk/src/bytecodes/index.ts
@@ -0,0 +1,7 @@
+// Byte codes
+export * from "./CredentialAtomicQuerySigValidatorByteCode";
+export * from "./NexeraVerifierEntrypointByteCode";
+export * from "./ScenarioVerifierByteCode";
+export * from "./ScenarioVerifierFactoryByteCode";
+export * from "./VerifierEntrypointFactoryByteCode";
+export * from "./VerifierSigWrapperByteCode";
