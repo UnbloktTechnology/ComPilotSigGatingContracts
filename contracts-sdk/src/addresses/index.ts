cd98e8aab9958f73d97eca81df19f91547181309
Antoine Estienne <estienne.antoine@gmail.com>
2024-01-15 12:45:43 +0100

    reoganize contract sdk into mutiple entrypoints

---
+++ contracts-sdk/src/addresses/index.ts
@@ -0,0 +1,9 @@
+// Deployed addresses
+
+// Mumbai
+export * from "./ScenarioVerifierFactoryAddress_mumbai_dev";
+export * from "./ScenarioVerifierFactoryAddress_mumbai_staging";
+export * from "./ScenarioVerifierFactoryAddress_mumbai_prod";
+export * from "./VerifierEntrypointFactoryAddress_mumbai_dev";
+export * from "./VerifierEntrypointFactoryAddress_mumbai_staging";
+export * from "./VerifierEntrypointFactoryAddress_mumbai_prod";
