7bed45169709d030a2cbbe4fd662eb01eb527d16
Antoine Estienne <estienne.antoine@gmail.com>
2024-02-22 10:22:42 +0100

    export example nft address

--- contracts-sdk/src/addresses/index.ts
+++ contracts-sdk/src/addresses/index.ts
@@ -1,12 +1,23 @@
 // Deployed addresses
 
 // Mumbai
+
+// ScenarioVerifierFactory
 export * from "./ScenarioVerifierFactoryAddress_mumbai_dev";
 export * from "./ScenarioVerifierFactoryAddress_mumbai_staging";
 export * from "./ScenarioVerifierFactoryAddress_mumbai_prod";
+
+// VerifierEntrypointFactory
 export * from "./VerifierEntrypointFactoryAddress_mumbai_dev";
 export * from "./VerifierEntrypointFactoryAddress_mumbai_staging";
 export * from "./VerifierEntrypointFactoryAddress_mumbai_prod";
+
+// ExampleGatedNFTMinter
 export * from "./ExampleGatedNFTMinterAddress_mumbai_dev";
 export * from "./ExampleGatedNFTMinterAddress_mumbai_staging";
 export * from "./ExampleGatedNFTMinterAddress_mumbai_prod";
+
+// ExampleNFTMinter
+export * from "./ExampleNFTMinterAddress_mumbai_dev";
+export * from "./ExampleNFTMinterAddress_mumbai_staging";
+export * from "./ExampleNFTMinterAddress_mumbai_prod";
