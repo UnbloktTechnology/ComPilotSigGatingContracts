ca47b306fbbe5d117f56bc8bce6c390494fb4ca5
Antoine Estienne <estienne.antoine@gmail.com>
2024-04-05 15:17:25 +0200

    add missing addresses

--- contracts-sdk/src/addresses/index.ts
+++ contracts-sdk/src/addresses/index.ts
@@ -12,6 +12,28 @@ export * from "./ExampleNFTMinterAddress_mumbai_dev";
 export * from "./ExampleNFTMinterAddress_mumbai_staging";
 export * from "./ExampleNFTMinterAddress_mumbai_prod";
 
+// NexeraIDSignerManager
+export * from "./NexeraIDSignerManagerAddress_mumbai_dev";
+export * from "./NexeraIDSignerManagerAddress_mumbai_staging";
+export * from "./NexeraIDSignerManagerAddress_mumbai_prod";
+
+// Amoy
+
+// ExampleGatedNFTMinter
+export * from "./ExampleGatedNFTMinterAddress_amoy_dev";
+export * from "./ExampleGatedNFTMinterAddress_amoy_staging";
+export * from "./ExampleGatedNFTMinterAddress_amoy_prod";
+
+// ExampleNFTMinter
+export * from "./ExampleNFTMinterAddress_amoy_dev";
+export * from "./ExampleNFTMinterAddress_amoy_staging";
+export * from "./ExampleNFTMinterAddress_amoy_prod";
+
+// NexeraIDSignerManager
+export * from "./NexeraIDSignerManagerAddress_amoy_dev";
+export * from "./NexeraIDSignerManagerAddress_amoy_staging";
+export * from "./NexeraIDSignerManagerAddress_amoy_prod";
+
 // Sepolia
 
 // ExampleGatedNFTMinter
@@ -23,3 +45,8 @@ export * from "./ExampleGatedNFTMinterAddress_sepolia_prod";
 export * from "./ExampleNFTMinterAddress_sepolia_dev";
 export * from "./ExampleNFTMinterAddress_sepolia_staging";
 export * from "./ExampleNFTMinterAddress_sepolia_prod";
+
+// NexeraIDSignerManager
+export * from "./NexeraIDSignerManagerAddress_sepolia_dev";
+export * from "./NexeraIDSignerManagerAddress_sepolia_staging";
+export * from "./NexeraIDSignerManagerAddress_sepolia_prod";
