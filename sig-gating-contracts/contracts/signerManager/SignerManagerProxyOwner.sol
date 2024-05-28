34721fdefbd0470c05399d5b2b16d16b73babffd
Antoine Estienne <estienne.antoine@gmail.com>
2024-05-28 14:35:00 +0200

    add SignerManagerProxyOwner and INexeraIDSignerManager

---
+++ sig-gating-contracts/contracts/signerManager/SignerManagerProxyOwner.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import {INexeraIDSignerManager} from "./INexeraIDSignerManager.sol";
+
+contract SignerManagerProxyOwner is AccessControl {
+    INexeraIDSignerManager public signerManager;
+
+    bytes32 public constant SIGNER_MANAGER_CONTROLLER_ROLE =
+        keccak256("SIGNER_MANAGER_CONTROLLER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor(address signerManagerAddress) {
+        signerManager = INexeraIDSignerManager(signerManagerAddress);
+
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(SIGNER_MANAGER_CONTROLLER_ROLE, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
+    }
+
+    // Function to set a new signer in the SignerManager contract
+    function setSigner(
+        address newSigner
+    ) external onlyRole(SIGNER_MANAGER_CONTROLLER_ROLE) {
+        signerManager.setSigner(newSigner);
+    }
+
+    // Function to transfer ownership of the SignerManager contract
+    function transferOwnership(
+        address newOwner
+    ) external onlyRole(SIGNER_MANAGER_CONTROLLER_ROLE) {
+        signerManager.transferOwnership(newOwner);
+    }
+
+    // Function to change the SIGNER_MANAGER_CONTROLLER_ROLE
+    function changeSignerManagerControllerRole(
+        address account
+    ) external onlyRole(SIGNER_MANAGER_CONTROLLER_ROLE) {
+        grantRole(SIGNER_MANAGER_CONTROLLER_ROLE, account);
+    }
+
+    // Function to change the PAUSER_ROLE
+    function changePauserRole(
+        address account
+    ) external onlyRole(SIGNER_MANAGER_CONTROLLER_ROLE) {
+        grantRole(PAUSER_ROLE, account);
+    }
+
+    // Function to pause the SignerManager by setting the signer to a null address
+    function pauseSignerManager() external onlyRole(PAUSER_ROLE) {
+        signerManager.setSigner(address(0));
+    }
+}
