83f0974189c4e1429306409274bf1bdb3a44de59
Frank Hillard <frank.hillard@gmail.com>
2024-05-30 01:17:14 +0200

    add TS script to mint (but bug while unpacking)

--- tezos-sig-gating-contracts/scripts/mint_offchain.ts
+++ tezos-sig-gating-contracts/scripts/mint_offchain.ts
@@ -4,8 +4,15 @@ import { char2Bytes } from "@taquito/utils";
 import { saveContractAddress } from "./helper";
 import nftMinterContract from "../compiled/nftminter.json";
 import nftMinterAddress from "../deployments/nftminter";
+import { convert_timestamp, convert_key, convert_nat, convert_string, convert_address, convert_mint } from './convert';
 
-const RPC_ENDPOINT = "https://ghostnet.ecadinfra.com";
+const createKeccakHash = require('keccak')
+const RPC_ENDPOINT = "https://ghostnet.ecadinfra.com";// "https://oxfordnet.ecadinfra.com"; //
+
+function keccak256(data : string) {
+  return createKeccakHash('keccak256').update(data, 'hex').digest('hex')
+}
+
 
 async function main() {
   const Tezos = new TezosToolkit(RPC_ENDPOINT);
@@ -21,14 +28,55 @@ async function main() {
 
   
   try {
-    const args = {};
+    const functioncall_contract = "KT1AoU1mrLRSM2zouUVkvLz2UHo1on4UAFBF";
+    const functioncall_name = "%mint_offchain";
+    // const functioncall_params_bytes = "0507070a000000160000dd3a6a5b3d553afe28e786454bc813b3ba38a0dc0307";
+    const functioncall_params = {
+      owner: "tz1fon1Hp3eRff17X82Y3Hc2xyokz33MavFF",
+      token_id: "2"
+    };
+    const dataKey = "edpkuoQnnWMys1uS2eJrDkhPnizRNyQYBcsBsyfX4K97jVEaWKTXat";
+    const exp_date = "2025-01-01T00:00:00.00Z";
+    const nonce = "0";
+    const signature = "edsigu69KiiWBUztmWX1ihtoUaeCK62EGqKwTN27n4V3RrgeiPoc3Y9YdyRJkekXFz7zLUYceVkZmTe5WquwYKYSocG1u3nxNgg";
+    const userAddress = "tz1fon1Hp3eRff17X82Y3Hc2xyokz33MavFF";
+
+    const functioncall_contract_bytes = convert_address(functioncall_contract);
+    const functioncall_name_bytes = convert_string(functioncall_name);
+    const functioncall_params_bytes = convert_mint(functioncall_params.owner, functioncall_params.token_id);
+    console.log("functioncall_params_bytes: ", functioncall_params_bytes);
+    console.log(functioncall_params_bytes == "0507070a000000160000dd3a6a5b3d553afe28e786454bc813b3ba38a0dc0307");
+    const nonce_bytes = convert_nat(nonce);
+    const exp_date_bytes = convert_timestamp(exp_date);
+    const key_bytes = convert_key(dataKey);
+    const payload = key_bytes + nonce_bytes + exp_date_bytes + functioncall_contract_bytes + functioncall_name_bytes + functioncall_params_bytes;
+    const payload_hash = keccak256(payload);
+    console.log("payload_hash=", payload_hash);
+
+    const args = {
+      msgData: { 
+        0: payload_hash, 
+        1: nonce, 
+        2: exp_date, 
+        3: functioncall_contract, 
+        4: functioncall_name, 
+        5: functioncall_params_bytes, 
+        6: dataKey, 
+        7: signature
+      },
+      userAddress: userAddress
+    };
     const cntr = await Tezos.contract.at(nftMinterAddress);
-    const op = await cntr.methodsObject.mint_offchain(args).send();
+    // const op = await cntr.methodsObject.exec_offchain(args).send();
+    const tx = await cntr.methodsObject.exec_offchain(args).toTransferParams(); //({gasLimit:5000});
+    console.log("tx: ", tx);
+    const est = await Tezos.estimate.transfer(tx);
+    console.log("est: ", est);
     console.log(
-      `Waiting for Mint_offchain on ${nftMinterAddress} to be confirmed...`
+      `Waiting for Exec_offchain on ${nftMinterAddress} to be confirmed...`
     );
-    await op.confirmation(2);
-    console.log("confirmed mint: ", op.hash);
+    // await op.confirmation(2);
+    // console.log("confirmed: signed message applied: ", op.hash);
   } catch (error: any) {
     console.log(error);
   }
--- tezos-sig-gating-contracts/scripts/signAuthData.ts
+++ tezos-sig-gating-contracts/scripts/signAuthData.ts
@@ -3,6 +3,8 @@ import { MichelsonMap, TezosToolkit } from "@taquito/taquito";
 import { verifySignature, stringToBytes } from "@taquito/utils";
 import { Parser, packDataBytes, MichelsonData, MichelsonType } from '@taquito/michel-codec';
 
+import { convert_timestamp, convert_key, convert_nat, convert_string, convert_address, convert_mint } from './convert';
+
 const createKeccakHash = require('keccak')
 // import verifierContract from "../compiled/TxAuthDataVerifier.json";
 
@@ -28,60 +30,60 @@ function keccak256(data : string) {
 }
 
 
-function convert_timestamp(tt : string) {
-  const data: MichelsonData = {
-      string: tt
-  };
-  const typ: MichelsonType = {
-      prim: "timestamp"
-  };
-  const packed = packDataBytes(data, typ);
-  return packed.bytes;
-}
+// function convert_timestamp(tt : string) {
+//   const data: MichelsonData = {
+//       string: tt
+//   };
+//   const typ: MichelsonType = {
+//       prim: "timestamp"
+//   };
+//   const packed = packDataBytes(data, typ);
+//   return packed.bytes;
+// }
 
-function convert_key(key_str : string) {
-  const data: MichelsonData = {
-      string: key_str
-  };
-  const typ: MichelsonType = {
-      prim: "key"
-  };
-  const packed = packDataBytes(data, typ);
-  return packed.bytes;
-}
+// function convert_key(key_str : string) {
+//   const data: MichelsonData = {
+//       string: key_str
+//   };
+//   const typ: MichelsonType = {
+//       prim: "key"
+//   };
+//   const packed = packDataBytes(data, typ);
+//   return packed.bytes;
+// }
 
-function convert_nat(nat_str : string) {
-  const data: MichelsonData = {
-      int: nat_str
-  };
-  const typ: MichelsonType = {
-      prim: "nat"
-  };
-  const packed = packDataBytes(data, typ);
-  return packed.bytes;
-}
+// function convert_nat(nat_str : string) {
+//   const data: MichelsonData = {
+//       int: nat_str
+//   };
+//   const typ: MichelsonType = {
+//       prim: "nat"
+//   };
+//   const packed = packDataBytes(data, typ);
+//   return packed.bytes;
+// }
 
-function convert_string(str : string) {
-  const data: MichelsonData = {
-      string: str
-  };
-  const typ: MichelsonType = {
-      prim: "string"
-  };
-  const packed = packDataBytes(data, typ);
-  return packed.bytes;
-}
+// function convert_string(str : string) {
+//   const data: MichelsonData = {
+//       string: str
+//   };
+//   const typ: MichelsonType = {
+//       prim: "string"
+//   };
+//   const packed = packDataBytes(data, typ);
+//   return packed.bytes;
+// }
 
-function convert_address(addr_str : string) {
-  const data: MichelsonData = {
-      string: addr_str
-  };
-  const typ: MichelsonType = {
-      prim: "address"
-  };
-  const packed = packDataBytes(data, typ);
-  return packed.bytes;
-}
+// function convert_address(addr_str : string) {
+//   const data: MichelsonData = {
+//       string: addr_str
+//   };
+//   const typ: MichelsonType = {
+//       prim: "address"
+//   };
+//   const packed = packDataBytes(data, typ);
+//   return packed.bytes;
+// }
 
 
 async function main() {
@@ -90,12 +92,17 @@ async function main() {
     const signerAddress = "tz1TiFzFCcwjv4pyYGTrnncqgq17p59CzAE2";
 
     // INPUTS
-    const functioncall_contract = "KT1PNgGEt3cnT9zvPnUWimX3KiAkFrX9wioE";
+    const functioncall_contract = "KT1AoU1mrLRSM2zouUVkvLz2UHo1on4UAFBF"; //"KT1PNgGEt3cnT9zvPnUWimX3KiAkFrX9wioE";
     const functioncall_name = "%mint_offchain";
-    const functioncall_params = "0507070a0000001600004c8408407ebb2be65120a765cd2cbf341b9860a70006";
+    // const functioncall_params_bytes = "0507070a000000160000dd3a6a5b3d553afe28e786454bc813b3ba38a0dc0307"; // "0507070a0000001600004c8408407ebb2be65120a765cd2cbf341b9860a70006";
+    const functioncall_params = {
+      owner: "tz1fon1Hp3eRff17X82Y3Hc2xyokz33MavFF",
+      token_id: "2"
+    };
     const dataKey = "edpkuoQnnWMys1uS2eJrDkhPnizRNyQYBcsBsyfX4K97jVEaWKTXat";
-    const exp_date = "1970-01-01T00:10:00.00Z";
+    const exp_date = "2025-01-01T00:00:00.00Z";
     const nonce = "0";
+    
 
     // display public key
     const signerPublicKey = await signer.publicKey();
@@ -104,13 +111,14 @@ async function main() {
     // display payload
     const functioncall_contract_bytes = convert_address(functioncall_contract);
     const functioncall_name_bytes = convert_string(functioncall_name);
+    const functioncall_params_bytes = convert_mint(functioncall_params.owner, functioncall_params.token_id);
     console.log("functioncall_name_bytes=", functioncall_name_bytes);
     const nonce_bytes = convert_nat(nonce);
     console.log("nonce_bytes=", nonce_bytes);
     const exp_date_bytes = convert_timestamp(exp_date);
     console.log("exp_date_bytes=", exp_date_bytes);
     const key_bytes = convert_key(dataKey);
-    const payload = key_bytes + nonce_bytes + exp_date_bytes + functioncall_contract_bytes + functioncall_name_bytes + functioncall_params;
+    const payload = key_bytes + nonce_bytes + exp_date_bytes + functioncall_contract_bytes + functioncall_name_bytes + functioncall_params_bytes;
     console.log("payload=", payload);
     const payload_hash = keccak256(payload);
     console.log("payload_hash=", payload_hash);
