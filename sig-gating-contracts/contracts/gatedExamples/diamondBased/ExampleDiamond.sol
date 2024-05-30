2721f2a0eece8bd5821420103cf105deba5805cb
Antoine Estienne <estienne.antoine@gmail.com>
2024-05-30 17:39:52 +0200

    add diamond contract

---
+++ sig-gating-contracts/contracts/gatedExamples/diamondBased/ExampleDiamond.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol";
+
+/**
+ * @notice Implementation of solidstate diamond
+ */
+contract ExampleDiamond is SolidStateDiamond {
+
+}
