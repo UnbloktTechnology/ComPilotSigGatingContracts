708019baa8af4c1c1b04bf18293e82cee24d1f59
Antoine Estienne <estienne.antoine@gmail.com>
2024-05-31 15:12:48 +0200

    custom SIGNER_MANAGER_CONTROLLER for Sepolia

--- sig-gating-contracts/deploy/01_deployNexeraIDSignerManager.ts
+++ sig-gating-contracts/deploy/01_deployNexeraIDSignerManager.ts
@@ -1,6 +1,7 @@
 import { getNamedAccounts, ethers } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { getMultiSigAddress } from "../lib/getMultiSigAddress";
 
 const version = "0.1.6";
 const contractName = "NexeraIDSignerManager";
@@ -8,19 +9,27 @@ const testEnv = "testnet";
 const mainEnv = "mainnet";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { deployments } = hre;
+  const { deployments, getChainId } = hre;
   const { deploy } = deployments;
   const { deployer, txAuthSignerAddress, signerManagerController } =
     await getNamedAccounts();
 
-  // 1. Deploy NexeraIDSignerManager
+  // Deploy NexeraIDSignerManager
   console.log(`\n--------------------------------------------------------`);
   console.log(`Deploying ${contractName}...`);
   console.log(`\n--------------------------------------------------------`);
 
+  // Get the chain ID
+  const chainId = await getChainId();
+  console.log(`Chain ID: ${chainId}`);
+
+  // Get SIGNER_MANAGER_CONTROLLER depending on network
+  const SIGNER_MANAGER_CONTROLLER =
+    getMultiSigAddress(chainId) || signerManagerController;
+
   console.log("deployer", deployer);
   console.log("txAuthSignerAddress", txAuthSignerAddress);
-  console.log("signerManagerController", signerManagerController);
+  console.log("SIGNER_MANAGER_CONTROLLER", SIGNER_MANAGER_CONTROLLER);
 
   const deployResult = await deploy(contractName, {
     contract: contractName,
@@ -28,7 +37,7 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
       (process.env.SALT || "SALT") + version
     ),
     from: deployer,
-    args: [txAuthSignerAddress, signerManagerController],
+    args: [txAuthSignerAddress, SIGNER_MANAGER_CONTROLLER],
     log: true,
     nonce: "pending",
     waitConfirmations: 1,
