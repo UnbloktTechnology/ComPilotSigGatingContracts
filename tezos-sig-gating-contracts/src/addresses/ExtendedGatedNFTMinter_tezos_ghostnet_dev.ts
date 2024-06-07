5fe9ee120d5ee32ccc61b7d887174b21ffbcbc94
Frank Hillard <frank.hillard@gmail.com>
2024-06-07 12:34:09 +0200

    Packaging

---
+++ tezos-sig-gating-contracts/src/addresses/ExtendedGatedNFTMinter_tezos_ghostnet_dev.ts
@@ -0,0 +1,4 @@
+// export default "KT1AJQC5SWN4sYPkUCmEyCbcpSFGeT1LF9sL";
+
+export const ExtendedGatedNFTMinterAddress_tezos_ghostnet_dev =
+  "KT1AJQC5SWN4sYPkUCmEyCbcpSFGeT1LF9sL" as const;
---
+++ tezos-sig-gating-contracts/src/contracts/index.ts
@@ -0,0 +1,6 @@
+// ABIs
+import nftMinterContract from "../../compiled/extended_gated_nftminter.json";
+// export default nftMinterContract
+export const ExtendedGatedNFTMinterContract_tezos_ghostnet_dev =
+    nftMinterContract;
+
