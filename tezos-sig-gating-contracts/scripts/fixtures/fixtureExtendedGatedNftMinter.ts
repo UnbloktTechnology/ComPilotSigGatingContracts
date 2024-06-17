00aa63a9155c849184fa45d45f94adeea5020351
Antoine Estienne <estienne.antoine@gmail.com>
2024-06-17 15:05:16 +0200

    add linting

--- tezos-sig-gating-contracts/scripts/fixtures/fixtureExtendedGatedNftMinter.ts
+++ tezos-sig-gating-contracts/scripts/fixtures/fixtureExtendedGatedNftMinter.ts
@@ -4,7 +4,7 @@ import { char2Bytes } from "@taquito/utils";
 import { saveContractAddress } from "../utils/helper";
 import nftMinterContract from "../../compiled/extended_gated_nftminter.json";
 
-const RPC_ENDPOINT = "http://localhost:20000/"; 
+const RPC_ENDPOINT = "http://localhost:20000/";
 
 export async function deployNFTMinterExt() {
   const Tezos = new TezosToolkit(RPC_ENDPOINT);
@@ -38,7 +38,7 @@ export async function deployNFTMinterExt() {
   token_metadata.set(1, { token_id: 1, token_info: token_info_1 });
   token_metadata.set(2, { token_id: 2, token_info: token_info_2 });
   token_metadata.set(3, { token_id: 3, token_info: token_info_3 });
-  
+
   const metadata = new MichelsonMap();
   metadata.set("", char2Bytes("tezos-storage:data"));
   metadata.set(
@@ -61,8 +61,8 @@ export async function deployNFTMinterExt() {
   const operators = new MichelsonMap();
 
   const fa2Extension = {
-    minter : "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb", // alice
-  }
+    minter: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb", // alice
+  };
 
   const initialFA2Storage = {
     extension: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb", //fa2Extension,
@@ -73,9 +73,9 @@ export async function deployNFTMinterExt() {
   };
 
   const initialStorage = {
-    admin : "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb", // alice
-    signerAddress : "tz1aSkwEot3L2kmUvcoxzjMomb9mvBNuzFK6", // bob
-    nonces : new MichelsonMap(),
+    admin: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb", // alice
+    signerAddress: "tz1aSkwEot3L2kmUvcoxzjMomb9mvBNuzFK6", // bob
+    nonces: new MichelsonMap(),
     siggated_extension: initialFA2Storage,
   };
 
@@ -89,7 +89,10 @@ export async function deployNFTMinterExt() {
     );
     await originated.confirmation(2);
     console.log("confirmed contract: ", originated.contractAddress);
-    await saveContractAddress("nftminter", originated?.contractAddress ?? "error");
+    await saveContractAddress(
+      "nftminter",
+      originated?.contractAddress ?? "error"
+    );
     return originated.contractAddress;
   } catch (error: any) {
     console.log(error);
