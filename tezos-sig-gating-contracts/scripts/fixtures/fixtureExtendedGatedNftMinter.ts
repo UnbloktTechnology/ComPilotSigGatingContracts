8fef3a97d16af2930544c301a5c4b00816d9d6bd
Frank Hillard <frank.hillard@gmail.com>
2024-06-17 20:55:59 +0200

    fix helper for nftminter initial storage

--- tezos-sig-gating-contracts/scripts/fixtures/fixtureExtendedGatedNftMinter.ts
+++ tezos-sig-gating-contracts/scripts/fixtures/fixtureExtendedGatedNftMinter.ts
@@ -21,39 +21,12 @@ export async function deployNFTMinterExt() {
   ledger.set(0, "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb");
 
   const token_metadata = new MichelsonMap();
-  const token_info_0 = new MichelsonMap();
-  token_info_0.set("name", char2Bytes("Token 0"));
-  token_info_0.set("description", char2Bytes("asset #0"));
-  const token_info_1 = new MichelsonMap();
-  token_info_1.set("name", char2Bytes("Token 1"));
-  token_info_1.set("description", char2Bytes("asset #1"));
-  const token_info_2 = new MichelsonMap();
-  token_info_2.set("name", char2Bytes("Token 2"));
-  token_info_2.set("description", char2Bytes("asset #2"));
-  const token_info_3 = new MichelsonMap();
-  token_info_3.set("name", char2Bytes("Token 3"));
-  token_info_3.set("description", char2Bytes("asset #3"));
-  const token_info_4 = new MichelsonMap();
-  token_info_4.set("name", char2Bytes("Token 4"));
-  token_info_4.set("description", char2Bytes("asset #4"));
-  const token_info_5 = new MichelsonMap();
-  token_info_5.set("name", char2Bytes("Token 5"));
-  token_info_5.set("description", char2Bytes("asset #5"));
-  const token_info_6 = new MichelsonMap();
-  token_info_6.set("name", char2Bytes("Token 6"));
-  token_info_6.set("description", char2Bytes("asset #6"));
-  const token_info_7 = new MichelsonMap();
-  token_info_7.set("name", char2Bytes("Token 7"));
-  token_info_7.set("description", char2Bytes("asset #7"));
-
-  token_metadata.set(0, { token_id: 0, token_info: token_info_0 });
-  token_metadata.set(1, { token_id: 1, token_info: token_info_1 });
-  token_metadata.set(2, { token_id: 2, token_info: token_info_2 });
-  token_metadata.set(3, { token_id: 3, token_info: token_info_3 });
-  token_metadata.set(4, { token_id: 4, token_info: token_info_4 });
-  token_metadata.set(5, { token_id: 5, token_info: token_info_5 });
-  token_metadata.set(6, { token_id: 6, token_info: token_info_6 });
-  token_metadata.set(7, { token_id: 7, token_info: token_info_7 });
+  for (let i = 0; i < 10; i++) {
+    const token_info_i = new MichelsonMap();
+    token_info_i.set("name", char2Bytes("Token " + i.toString()));
+    token_info_i.set("description", char2Bytes("asset #" + i.toString()));
+    token_metadata.set(i, { token_id: i, token_info: token_info_i });
+  }
 
   const metadata = new MichelsonMap();
   metadata.set("", char2Bytes("tezos-storage:data"));
