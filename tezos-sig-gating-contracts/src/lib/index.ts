e6d020ae76e3afdb89ce3a9ef9bc5af4d459ef3d
Frank Hillard <frank.hillard@gmail.com>
2024-06-25 17:51:54 +0200

    Merge main (fix)

--- tezos-sig-gating-contracts/src/lib/index.ts
+++ tezos-sig-gating-contracts/src/lib/index.ts
@@ -1,8 +1,5 @@
 export * from "./schemas";
 export * from "./convert";
-<<<<<<< HEAD
-=======
 export * from "./buildTxCallData";
 export * from "./computePayloadHash";
->>>>>>> main
 export * from "./signTxAuthDataTezos";
