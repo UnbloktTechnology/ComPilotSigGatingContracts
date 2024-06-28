a6773ec58e744349829e74507bfdf29e0edd495b
Frank Hillard <frank@nexera.id>
2024-06-28 16:41:38 +0200

    remove old files (renaming)

--- tezos-sig-gating-contracts/src/compiledcontracts/index.ts
+++
@@ -1,5 +0,0 @@
-// ABIs
-import nftMinterContract from "./gatednftminter_simple.json";
-// export default nftMinterContract
-export const ExtendedGatedNFTMinterContract_tezos_ghostnet_dev =
-  nftMinterContract;
