3980d0edcffe49f7479159fffaf277a8c4d295f7
Frank Hillard <frank@nexera.id>
2024-07-10 12:57:56 +0200

    add smart contract examples in sdk

--- tezos-sig-gating-contracts/src/compiledContracts/index.ts
+++ tezos-sig-gating-contracts/src/compiledContracts/index.ts
@@ -1,10 +1,14 @@
 // ABIs
-import nftMinterSimpleContract from "./gatedNftMinterSimple.json";
+import nftClaimerContract from "./gatedNftClaimer.json";
 import nftMinterSimpleUnlimitedContract from "./gatedNftMinterSimpleUnlimited.json";
+import nftMinterMultiContract from "./gatedNftMinterMulti.json";
 
-// export default nftMinterSimpleContract
-export const NFTMinterSimpleContractForTezos = nftMinterSimpleContract;
+// export default nftClaimerContract
+export const NFTClaimerContractForTezos = nftClaimerContract;
 
 // export default nftMinterSimpleUnlimitedContract
 export const NFTMinterSimpleUnlimitedContractForTezos =
   nftMinterSimpleUnlimitedContract;
+
+// export default nftClaimerContract
+export const NFTMinterMultiContractForTezos = nftMinterMultiContract;
